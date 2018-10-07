236be7e1bc04b682feae095d44528f371b75198e
pro2s <protasov.s.s@gmail.com>
2018-10-07 10:56:04 +0300

    install decomposition

---
+++ doika/install/steps/folder-permisson.php
@@ -0,0 +1,31 @@
+<?php
+    $folders = array (
+        'storage/framework/'     => '775',
+        'storage/logs/'          => '775',
+        'bootstrap/cache/'       => '775',
+        'public/images/'         => '775'
+    );
+    if (file_exists($install_folder)) {
+        ?>
+        <table>
+            <tbody>
+                <?php
+                foreach ($folders as $folder => $permission) {
+                    echo "<tr><th>$install_folder$folder</th><td>";
+                    if (intval(substr(sprintf('%o', fileperms($install_folder. $folder)), -3)) >= intval($permission)) {
+                        echo "&#10004;";
+                    } else {
+                        echo "&#10008;";
+                        $error = true;
+                    }
+                    echo "</td></tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        <?php
+    } else {
+        echo "Папка $install_folder не существует. Попробуйте заново скачать и разархивировать <a href='https://github.com/cema93/doika-instalator/archive/master.zip' target='_blank'>архив</a> в корень вашего сайта. ";
+        $error = true;
+    }
+?>
---
+++ doika/install/steps/system-requirements.php
@@ -0,0 +1,66 @@
+<?php
+    $php_version = '7.0.0';
+    $php_extensions = array (
+        'openssl',
+        'pdo',
+        'mbstring',
+        'tokenizer',
+        'JSON',
+        'cURL',
+    );
+?>
+<table>
+    <thead>
+        <tr>
+            <th>PHP</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>PHP версии <?php echo $php_version; ?> или выше </th>
+            <td>
+            <?php
+            if (version_compare(PHP_VERSION, $php_version) >= 0) {
+                echo "&#10004;";
+            } else {
+                $error = true;
+                echo "#10008;";
+            }
+            ?>
+            </td>
+        </tr>
+
+        <?php
+        foreach ($php_extensions as $extension) {
+            echo "<tr><th>$extension</th><td>";
+            if (extension_loaded($extension)) {
+                echo "&#10004;";
+            } else {
+                echo "&#10008;";
+                $error = true;
+            }
+            echo "</td></tr>";
+        }
+        ?>
+    </tbody>
+    <thead>
+        <tr>
+            <th>Apache</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>mod_rewrite</th>
+            <td>
+                <?php
+                if (function_exists('apache_get_modules')) {
+                    echo "&#10004;";
+                } else {
+                    echo "&#10008;";
+                    $error = true;
+                }
+                ?>
+            </td>
+        </tr>
+    </tbody>
+</table>
