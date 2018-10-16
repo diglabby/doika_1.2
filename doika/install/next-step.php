62e067a7f89151e1d06f609b4c3ee8137c3ffb5b
pro2s <protasov.s.s@gmail.com>
2018-10-17 00:47:30 +0300

    revert format

--- doika/install/next-step.php
+++ doika/install/next-step.php
@@ -1,7 +1,7 @@
-<?php 
-    if (!$error) {
-        echo '<p class="step"><a href="' . $nextStep . '" class="button button-large">Вперёд!</a></p>';
-    } else {
-        echo "<p class='error'>Вы сможете продолжить после того,как исправите ошибки</p>";
-        echo '<p class="step"><a href="' . $repeatStep . '" class="button button-large">Повторить проверку</a></p>';
-    }
+<?php
+if (!$error) {
+    echo '<p class="step"><a href="' . $nextStep . '" class="button button-large">Вперёд!</a></p>';
+} else {
+    echo "<p class='error'>Вы сможете продолжить после того,как исправите ошибки</p>";
+    echo '<p class="step"><a href="' . $repeatStep . '" class="button button-large">Повторить проверку</a></p>';
+}
